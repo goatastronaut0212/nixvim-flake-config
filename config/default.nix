64e27e269879118269d45d9d37aa3695445fab2e
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-01-01 20:45:03 +0700

    Add viAlias, vimAlias and files tabs set

--- config/default.nix
+++ config/default.nix
@@ -1,4 +1,30 @@
 {
+  viAlias = true;
+  vimAlias = true;
+  files = {
+    "ftplugin/lua.lua" = {
+      opts = {
+        expandtab = true;
+        shiftwidth = 4;
+        tabstop = 4;
+      };
+    };
+    "ftplugin/nix.lua" = {
+      opts = {
+        expandtab = true;
+        shiftwidth = 2;
+        tabstop = 2;
+      };
+    };
+    "ftplugin/python.lua" = {
+      opts = {
+        expandtab = true;
+        shiftwidth = 4;
+        tabstop = 4;
+      };
+    };
+  };
+
   # Import all your configuration modules here
   imports = [
     ./bufferline.nix
