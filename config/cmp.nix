dc44901221d252bcfcc9cc197c0ed239c6501016
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-01-02 15:19:39 +0700

    Add cmp-nvim-lsp, cmp-path, cmp-buffer to cmp

--- config/cmp.nix
+++ config/cmp.nix
@@ -1,10 +1,17 @@
 {
-  plugins.cmp = {
-    autoEnableSources = true;
-    settings.sources = [
-      { name = "nvim_lsp"; }
-      { name = "path"; }
-      { name = "buffer"; }
-    ];
+  plugins = {
+    cmp = {
+      enable = true;
+      autoLoad = true;
+      autoEnableSources = true;
+      settings.sources = [
+        { name = "nvim_lsp"; }
+        { name = "path"; }
+        { name = "buffer"; }
+      ];
+    };
+    cmp-nvim-lsp.enable = true;
+    cmp-path.enable = true;
+    cmp-buffer.enable = true;
   };
 }
