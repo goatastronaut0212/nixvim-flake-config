2eee2738ed6780f6e32c164d9bf904f88598a34a
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-01-02 10:11:30 +0700

    Add FzfLua keymaps

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -8,5 +8,29 @@
         silent = true;
       };
     }
+    {
+      action = "<cmd>FzfLua files<CR>";
+      key = "<leader>ff";
+      mode = "n";
+      options = {
+        silent = true;
+      };
+    }
+    {
+      action = "<cmd>FzfLua buffers<CR>";
+      key = "<leader>fb";
+      mode = "n";
+      options = {
+        silent = true;
+      };
+    }
+    {
+      action = "<cmd>FzfLua live_grep<CR>";
+      key = "<leader>fg";
+      mode = "n";
+      options = {
+        silent = true;
+      };
+    }
   ];
 }
